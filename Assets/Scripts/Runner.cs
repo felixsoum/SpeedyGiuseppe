87779fb347f38b9a8628cc146604e8b1b8ed56c0
Derek Scheunemann <Derek.Scheunemann@gmail.com>
2014-10-25 18:59:06 -0400

    LOG:
    1.Checked Fixed angle box on runners rigidBody2D component to prevent Rotations
    
    2. Set runner's tag to "Player"
    
    3.Allowed for small Jumps if keyboard if spacebar is released early
    
    4. Changed from using jump boolean and count variable to touchingPlatform for grounded detection
    
    5. Added a physics Material to the platform Object and Runner prefab to allow for friction
    
    6. Added "Terrain" Tag to platform prefab for more detailed collision Handling
    
    7.Changed Runner's movement to work with Force/Friction

--- Assets/Scripts/Runner.cs
+++ Assets/Scripts/Runner.cs
@@ -6,6 +6,7 @@ public class Runner : MonoBehaviour {
 	public static float distanceTraveled;
 	private bool touchingPlatform;
 	public static int boosts;
+	
 
 	public float maxSpeed = 1f;
 	BoxCollider2D boxCollider;
@@ -13,10 +14,15 @@ public class Runner : MonoBehaviour {
 	bool run = false;
 	int counter = 60;
 	public float gameOverY;
+	public float jumpPower;
+	public float movementPower;
+
 
 	private Vector3 startPosition;
 	
 	void Start () {
+		movementPower = 500f;
+		jumpPower = 500;
 		GameEventManager.GameStart += GameStart;
 		GameEventManager.GameOver += GameOver;
 		startPosition = transform.localPosition;
@@ -27,50 +33,70 @@ public class Runner : MonoBehaviour {
 
 	void FixedUpdate()
 	{
-		float move = Input.GetAxis("Horizontal");
-		if (move > 0 || move < 0) {
-			if(maxSpeed >= 10f)
-			{
-				if(maxSpeed >= 40f)
-				{
-					maxSpeed+= 0.001f;
-				}
-				else
-				{
-					maxSpeed+= 0.1f;
-				}
-			}
-			else
-			{
-				print(maxSpeed);
-				maxSpeed+=1f;
-			}
-		} else {
-			maxSpeed = 1f;
-		}
-		rigidbody2D.velocity = new Vector2(move * maxSpeed, rigidbody2D.velocity.y);
-		
-		if(counter <= 0)
-		{
-			jump = false;
-		}
-		counter--;
+//		float move = Input.GetAxis("Horizontal");
+//		if (move > 0 || move < 0) {
+//			if(maxSpeed >= 10f)
+//			{
+//				if(maxSpeed >= 40f)
+//				{
+//					maxSpeed+= 0.001f;
+//				}
+//				else
+//				{
+//					maxSpeed+= 0.1f;
+//				}
+//			}
+//			else
+//			{
+//				print(maxSpeed);
+//				maxSpeed+=1f;
+//			}
+//		} else {
+//			maxSpeed = 1f;
+//		}
+//		rigidbody2D.velocity = new Vector2(move * maxSpeed, rigidbody2D.velocity.y);
+//		
+//		if(counter <= 0)
+//		{
+//			jump = false;
+//		}
+//		counter--;
 	}
 	
 	void Update()
 	{
-		if (Input.GetKeyDown (KeyCode.Space) && (jump == false)) 
+		//Horizontal Movement only when grounded
+
+
+		//Jump only when Grounded
+		if (Input.GetKeyDown (KeyCode.Space) && (touchingPlatform == true)) 
 		{
-			rigidbody2D.AddForce(new Vector2(0,500f));
-			jump = true;
-			counter = 45;
-			maxSpeed = 1f;
+			rigidbody2D.AddForce(new Vector2(0,jumpPower));
+			touchingPlatform = false;
 		} 
+		//Allow for small jumps if spacebar is released early
+		else if(Input.GetKeyUp (KeyCode.Space) && rigidbody2D.velocity.y > 0){
+			rigidbody2D.velocity = new Vector2(rigidbody2D.velocity.x, 0);
+
+		}
+
+		//Uncomment/Comment in the following line to toggle control in the air
+		if(Input.GetAxis ("Horizontal") != 0 /*&& touchingPlatform == true*/) {
+			
+			//might need to modify this
+			rigidbody2D.AddForce(new Vector2(Input.GetAxis ("Horizontal"), 0) * movementPower * Time.deltaTime);
+		}
 		distanceTraveled = transform.localPosition.x;
+
+		//this line fucks shit up for some reason
+
 		
 		if(transform.localPosition.y < gameOverY){
 			GameEventManager.TriggerGameOver();
 		}
+
+
+
 
 	}
 
@@ -89,13 +115,18 @@ public class Runner : MonoBehaviour {
 		enabled = false;
 	}
 
-	void OnCollisionEnter () {
+	void OnCollisionEnter2D (Collision2D col) {
+		if(col.gameObject.tag == "Terrain"){
 		touchingPlatform = true;
+		}
 	}
 	
-	void OnCollisionExit () {
-		touchingPlatform = false;
+	void OnCollisionExit2D (Collision2D col) {
+		if(col.gameObject.tag == "Terrain"){
+			touchingPlatform = false;
+		}
 	}
+
 
 	public static void AddBoost () {
 		boosts += 1;
