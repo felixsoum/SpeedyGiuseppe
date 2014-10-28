34c1ee8603273fce5c85907389338b6b21ab81a6
Lawrence Celis <cosmosferah316@gmail.com>
2014-10-27 22:10:57 -0400

    Added temporary main character sprites and animation.

---
+++ Assets/MainCamera.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainCamera : MonoBehaviour {
+
+	public Transform focus;
+	
+	void Update () {
+		Vector3 nextPosition = transform.position;
+		nextPosition.x = focus.position.x;
+		transform.position = nextPosition;
+	}
+}
--- Assets/Scripts/Runner.cs
+++ Assets/Scripts/Runner.cs
@@ -2,24 +2,36 @@
 using System.Collections;
 
 public class Runner : MonoBehaviour {
-	
-	public static float distanceTraveled;
-	private bool touchingPlatform;
-	public static int boosts;
-	
 
-	public float maxSpeed = 1f;
-	BoxCollider2D boxCollider;
-	bool jump = false;
-	bool run = false;
-	int counter = 60;
+	public Transform groundPoint;
+	//public Animation animation;
+	private bool touchingPlatform;
+	public static float distanceTraveled;
+	public static int boosts;
 	public float gameOverY;
 	public float jumpPower;
 	public float movementPower;
+	//public float maxSpeed = 1f;
 
+	BoxCollider2D boxCollider;
+	Animator animator;
+	bool jump = false;
+	bool run = false;
+	bool facingRight = true;
+	bool isGrounded = false;
+	float move;
+	int counter = 60;
 
 	private Vector3 startPosition;
-	
+
+	float acceleration = 1.0f;
+	float maxSpeed = 20.0f;
+
+	void Awake(){
+		animator = GetComponent<Animator>();
+		boxCollider = GetComponent<BoxCollider2D> ();
+	}
+
 	void Start () {
 		movementPower = 500f;
 		jumpPower = 500;
@@ -61,16 +73,34 @@ public class Runner : MonoBehaviour {
 //			jump = false;
 //		}
 //		counter--;
+		isGrounded = Physics2D.OverlapPoint (groundPoint.position);
+		animator.SetBool ("IsGrounded", isGrounded == true);
+		move = Input.GetAxis ("Horizontal");
+
+		animator.SetFloat ("Speed", Mathf.Abs (move));
+		//Uncomment/Comment in the following line to toggle control in the air
+		if(Input.GetAxis ("Horizontal") != 0 /*&& touchingPlatform == true*/) {
+			//might need to modify this
+			rigidbody2D.AddForce(new Vector2(move, 0) * movementPower * Time.deltaTime);	 
+
+			//animation["speedy_g_run"].speed = 2.0f;
+		}
+
+		if((move > 0 && !facingRight) || (move < 0 && facingRight)){
+			Flip();
+		}
 	}
 	
 	void Update()
 	{
+
 		//Horizontal Movement only when grounded
 
 
 		//Jump only when Grounded
-		if (Input.GetKeyDown (KeyCode.Space) && (touchingPlatform == true)) 
+		if (Input.GetKeyDown (KeyCode.Space) && (touchingPlatform == true) && isGrounded) 
 		{
+			animator.SetBool("IsGrounded", false);
 			rigidbody2D.AddForce(new Vector2(0,jumpPower));
 			touchingPlatform = false;
 		} 
@@ -80,12 +110,7 @@ public class Runner : MonoBehaviour {
 
 		}
 
-		//Uncomment/Comment in the following line to toggle control in the air
-		if(Input.GetAxis ("Horizontal") != 0 /*&& touchingPlatform == true*/) {
-			
-			//might need to modify this
-			rigidbody2D.AddForce(new Vector2(Input.GetAxis ("Horizontal"), 0) * movementPower * Time.deltaTime);
-		}
+
 		distanceTraveled = transform.localPosition.x;
 
 		//this line fucks shit up for some reason
@@ -94,10 +119,15 @@ public class Runner : MonoBehaviour {
 		if(transform.localPosition.y < gameOverY){
 			GameEventManager.TriggerGameOver();
 		}
+	
+	}
 
-
-
-
+	private void Flip()
+	{
+		facingRight = !facingRight;
+		Vector3 nextScale = transform.localScale;
+		nextScale.x *= -1f; 
+		transform.localScale = nextScale;	
 	}
 
 	private void GameStart () {
