5a0a8ba39d4b4008412b8298e5c564b7dbcf184c
David Siekut <davidsiekut@gmail.com>
2014-10-30 02:38:00 -0400

    Camera and sound fix.

---
+++ Assets/SpeedyCamera.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpeedyCamera : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        Vector3 pos = this.transform.position;
+        pos.z = -40f;
+        pos.y = 0f;
+        this.transform.position = pos;
+
+        int r = Random.Range(0, 100);
+        if (r == 0)
+        {
+            pos = this.transform.position;
+            pos.z = 5f;
+            pos.y = 40f;
+            this.transform.position = pos;
+
+        }
+	}
+}
